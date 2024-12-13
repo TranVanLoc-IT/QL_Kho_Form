2f9c3fe8eaad37f39ba840a7c0e577257fd8b786
dinhquyle <ledinhquy1509@gmail.com>
2024-12-13 19:37:56 +0700

    chức năng xuất báo cáo tồn kho, xem các chi tiết phiếu nhập

---
+++ MWarehouse.Service/Service/BC_Phieunhap.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using MWarehouse.Contract.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWarehouse.Service.Service
+{
+    public class BC_Phieunhap
+    {
+        private readonly IUnitOfWork _iuow;
+
+
+        public BC_Phieunhap(IUnitOfWork iuow)
+        {
+            _iuow = iuow;
+        }
+
+        public DataTable GetBCphieuNhap(DateTime ngayNhap, DateTime ngayXuat)
+        {
+            DataTable baoCaoTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection("Server=DESKTOP-QBTN8I9\\SQLEXPRESS;Database=QL_Kho;User Id=sa;Password=123;Trust Server Certificate=True;"))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("[dbo].[sp_GetNhapKhoByDateRange]", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Thêm tham số cho thủ tục
+                        command.Parameters.Add(new SqlParameter("@DATEFROM", ngayNhap));
+                        command.Parameters.Add(new SqlParameter("@DATETO", ngayXuat));
+
+                        // Thực thi và lấy dữ liệu
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        adapter.Fill(baoCaoTable);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Lỗi khi lấy báo cáo phiếu nhập: {ex.Message}", ex);
+                }
+            }
+
+            return baoCaoTable;
+        }
+
+    }
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -18,6 +18,7 @@ namespace UI
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             var serviceCollection = new ServiceCollection();
